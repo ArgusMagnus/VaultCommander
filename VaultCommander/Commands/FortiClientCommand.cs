4cecf90d7263aa0bbfd3a7a322456ef80bf3cec3
ArgusMagnus <argusmagnus@outlook.com>
2024-06-05 14:43:20 +0200

    fix

--- VaultCommander/Commands/FortiClientCommand.cs
+++ VaultCommander/Commands/FortiClientCommand.cs
@@ -27,36 +27,36 @@ sealed class FortiClientCommand : Command<FortiClientCommand.Arguments>
     readonly string _exe = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), @"Fortinet\FortiClient\FortiClient.exe");
 
     public override string Name => "forticlient";
-    public override bool CanExecute => true;// File.Exists(_exe);
+    public override bool CanExecute => File.Exists(_exe);
     public override bool RequireDisconnect => true;
 
     public override async Task Execute(Arguments args)
     {
         await Task.Delay(0).ConfigureAwait(false);
-        //using (var regKey = Registry.CurrentUser.CreateSubKey(@"Software\Fortinet\FortiClient\FA_VPN", true))
-        //    regKey.SetValue("connection", args.VpnName ?? string.Empty);
+        using (var regKey = Registry.CurrentUser.CreateSubKey(@"Software\Fortinet\FortiClient\FA_VPN", true))
+            regKey.SetValue("connection", args.VpnName ?? string.Empty);
 
-        //using var process = Process.Start(new ProcessStartInfo { FileName = _exe, WorkingDirectory = Path.GetDirectoryName(_exe) })!;
-        //await Utils.WaitForProcessReady(process);
-        //WindowHandle windowHandle = new(process.MainWindowHandle);
-        //windowHandle.Focus();
-        //var maxEmailAge = DateTimeOffset.Now;
+        using var process = Process.Start(new ProcessStartInfo { FileName = _exe, WorkingDirectory = Path.GetDirectoryName(_exe) })!;
+        await Utils.WaitForProcessReady(process);
+        WindowHandle windowHandle = new(process.MainWindowHandle);
+        windowHandle.Focus();
+        var maxEmailAge = DateTimeOffset.Now;
         Utils.SendKeys($"{{TAB}}{{TAB}}{args.Username}{{TAB}}{args.Password}{{ENTER}}");
 
-        //var totp = args.Totp;
-        //if (args.Mail is not null)
-        //{
-        //    CancellationTokenSource cts = new();
-        //    cts.CancelAfter(TimeSpan.FromMinutes(2));
-        //    var result = await Utils.PollEmail(args.Mail, maxEmailAge, cts.Token);
-        //    totp = Regex.Match(result.Subject, @"\d+").Value;
-        //}
+        var totp = args.Totp;
+        if (args.Mail is not null)
+        {
+            CancellationTokenSource cts = new();
+            cts.CancelAfter(TimeSpan.FromMinutes(2));
+            var result = await Utils.PollEmail(args.Mail, maxEmailAge, cts.Token);
+            totp = Regex.Match(result.Subject, @"\d+").Value;
+        }
 
-        //if (!string.IsNullOrEmpty(totp))
-        //{
-        //    Clipboard.SetText(totp);
-        //    MessageBox.Show("Token wurde in die Zwischenablage kopiert", nameof(VaultCommander), MessageBoxButton.OK, MessageBoxImage.Information);
-        //}
+        if (!string.IsNullOrEmpty(totp))
+        {
+            Clipboard.SetText(totp);
+            MessageBox.Show("Token wurde in die Zwischenablage kopiert", nameof(VaultCommander), MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 
     public override async Task<bool> Disconnect()
